a40c9d31c41e53172d33c35660eef858ff6c43d3
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-06-03 17:07:12 +0700

    C2 Async Basics

---
+++ research/concurrency-in-c#/c2_async-basics/c2_async-basics/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+async Task<T> DelayResult<T>(T result, TimeSpan delay)
+{
+    await Task.Delay(delay);
+    return result;
+}
+
+Console.WriteLine("Start");
+string message = await DelayResult<string>("Called before delay", TimeSpan.FromSeconds(1000));
+Console.WriteLine(message);
