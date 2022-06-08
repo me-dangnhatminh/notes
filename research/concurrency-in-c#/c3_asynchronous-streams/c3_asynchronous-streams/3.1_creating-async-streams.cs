02a5723c8864e8ad004b0f0d6fc2387eb3f025dd
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-06-08 12:57:45 +0700

    Chapter 3. asynchronous streams start

---
+++ research/concurrency-in-c#/c3_asynchronous-streams/c3_asynchronous-streams/3.1_creating-async-streams.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c3_asynchronous_streams
+{
+    internal class CreatingAsynchronousStreams
+    {
+        public static async IAsyncEnumerable<int> GetValuesAsync()
+        {
+            await Task.Delay(1000); // some asynchronous work
+            yield return 10;
+            await Task.Delay(1000); // more asynchronous work
+            yield return 13;
+        }
+        public static async Task Run()
+        {
+            IAsyncEnumerable<int> values = GetValuesAsync();
+
+        }
+    }
+}
---
+++ research/concurrency-in-c#/c3_asynchronous-streams/c3_asynchronous-streams/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace c3_asynchronous_streams
+{
+    /** Asynchronous Stream
+    * Các luồng không đồng bộ là một cách để nhận nhiều mục dữ liệu (data items) một cách không đồng bộ,
+    * chúng được xây dựng dựa trên asynchronous enumerable(IAsyncEnumerable<T>).
+    * Một kiểu async-enumerable là một version của enumerable, điều này có nghĩa là nó có thể sản xuất các mặt hàng theo yêu cầu cho người tiêu dùng
+    * và mỗi mặt hằng có thể được sản xuất không đồng bộ.
+    ** Asynchronous stream and Task
+    * Asynchronous stream giống với kiểu enumerable. Cụ thể, một IAsyncEnumerator<T> có thể cung cấp bất kỳ số lượng T giá trị nào tại một thời điểm.
+    ** Asynchronous stream and IEnumerable<T>
+    ** Asynchronous stream and IObservable<T>
+    */
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreatingAsynchronousStreams.Run();
+            Console.ReadKey();
+        }
+    }
+}
