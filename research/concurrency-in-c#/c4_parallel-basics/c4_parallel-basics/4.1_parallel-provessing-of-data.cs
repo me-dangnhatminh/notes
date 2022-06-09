37d02d022b220c648f7b5d939f2f96bd2a4681b9
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-06-09 09:54:02 +0700

    Chapter 4: - 4.1: Parallel.ForEach, lock, and package PLINQ

---
+++ research/concurrency-in-c#/c4_parallel-basics/c4_parallel-basics/4.1_parallel-provessing-of-data.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c4_parallel_basics
+{
+    /*
+     * Discussion
+     *  Parallel.ForEach cho cho phép sử lý song song trên một chuỗi giá trị. Một phương pháp tương tự là LINQ Parallel(PLINQ),
+     *  cung cấp nhiều khả năng tương tự. Một điểm khác duy nhất là, PLINQ bảo rằng nó có thể sử dụng tất cả các lõi trên máy tính,
+     *  trong khi parallel sẽ phản ứng động với các điều kiện thay đổi của CPU.
+     */
+    public class Matrix
+    {
+        public void Rotate(float degrees)
+        {
+            Console.WriteLine(degrees);
+        }
+    }
+    internal class ParallelProcessingofData
+    {
+        static async Task RotateMatrices(IEnumerable<Matrix> matrices, float degrees)
+        {
+            Console.WriteLine("start");
+            Parallel.ForEach(matrices, (matrix, state) =>
+            {
+
+                state.Stop(); //Khi gọi stop nó sẽ không gọi những thread chưa được gọi, nghĩa là sẽ có nhưng thread đã được gọi, bao gôm cả cái hiện tại sẽ vẫn tiếp tục chạy
+                matrix.Rotate(123);
+            });
+            Console.WriteLine("end");
+        }
+
+        static async Task RotateMatrices(IEnumerable<Matrix> matrices, float degrees, CancellationToken cancellationToken)
+        {
+            Parallel.ForEach(matrices, new ParallelOptions { CancellationToken = cancellationToken }, matrix =>
+            {
+                matrix.Rotate(degrees);
+            });
+        }
+
+        // Note: Đây không phải là cách triển khai hiệu quả
+        static void ShowNumberParallel( IEnumerable<int> numbers)
+        {
+            object i = 0;
+            Parallel.ForEach(numbers, ( num, state ) =>
+            {
+                lock(i)
+                {
+                    int index = Int32.Parse(i.ToString());
+                    Console.WriteLine($"[{index}]: " + num);
+                    i = index + 1;
+                }
+            });
+        }
+        public static async Task Run()
+        {
+            //IEnumerable<Matrix> matrices = new List<Matrix>()
+            //{
+            //    new Matrix(),
+            //    new Matrix(),
+            //    new Matrix(),
+            //    new Matrix(),
+            //};
+            //RotateMatrices(matrices, 12);
+
+            //IEnumerable<int> nums = new List<int>() { 1,2,3,4,5,6,7,8,9,10 };
+            //ShowNumberParallel(nums);
+        }
+    }
+}
---
+++ research/concurrency-in-c#/c4_parallel-basics/c4_parallel-basics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c4_parallel_basics
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            ParallelProcessingofData.Run();
+            Console.ReadKey();
+        }
+    }
+}
